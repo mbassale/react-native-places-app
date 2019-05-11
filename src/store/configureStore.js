099222b314c765e2bf3489078b417c6987e85a55
Marco Bassaletti <bassaletti@gmail.com>
2019-05-11 18:49:54 -0400

    Prevent error when there is no redux devtools loaded.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,7 +6,7 @@ const rootReducer = combineReducers({
 });
 
 let composeEnhancers = compose;
-if (__DEV__) {
+if (__DEV__ && typeof(__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) !== 'undefined') {
     composeEnhancers = __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
 
